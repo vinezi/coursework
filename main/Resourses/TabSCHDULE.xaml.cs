6acbbb62e86251edf0c38f76bd70b5b92cb3f6fb
BlackChep1 <kolynazaza@gmail.com>
2020-12-20 15:29:15 +0300

    2 commit

---
+++ main/Resourses/TabSCHDULE.xaml.cs
@@ -0,0 +1,97 @@
+﻿using main.Models;
+using main.Servesice;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace main.Resourses
+{
+    /// <summary>
+    /// Логика взаимодействия для TabSCHDULE.xaml
+    /// </summary>
+    public partial class TabSCHDULE : UserControl
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\testDate";
+        private BindingList<Schedule> _todoData;
+        private FileIOService _fileIOService;
+
+        public TabSCHDULE()
+        {
+            InitializeComponent();
+            SelectDay.SelectedIndex = 0;
+            ListBoxItem FirstItem = new ListBoxItem();
+            FirstItem = (ListBoxItem)SelectDay.SelectedItem;
+            FirstItem.Focus();
+        }
+        private void SelectDay_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string SelectedDay = ((ListBoxItem)SelectDay.SelectedItem).Tag.ToString();
+            _fileIOService = new FileIOService(PATH + SelectedDay + ".json");
+            _todoData = _fileIOService.LoadDaete();
+            try
+            {
+                _todoData.ListChanged += _todoData_ListChanged;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            test.ItemsSource = _todoData;
+            _todoData.ListChanged += _todoData_ListChanged;
+        }
+
+        private void _todoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveDate(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void TextTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox TextTime = sender as TextBox;
+            if (Regex.IsMatch(TextTime.Text, "[^0-9:]"))
+            {
+                TextTime.Text = TextTime.Text.Remove(TextTime.Text.Length - 1);
+                TextTime.SelectionStart = TextTime.Text.Length;
+            }
+        }
+        private void TextCabinet_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox TextCabinet = sender as TextBox;
+            if (Regex.IsMatch(TextCabinet.Text, "[^0-9/]"))
+            {
+                TextCabinet.Text = TextCabinet.Text.Remove(TextCabinet.Text.Length - 1);
+                TextCabinet.SelectionStart = TextCabinet.Text.Length;
+            }
+        }
+
+        private void TextBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.Focus();
+            if (textBox.Text != null) textBox.SelectAll();
+        }
+    }
+}
---
+++ main/Servesice/FileIOService.cs
@@ -0,0 +1,50 @@
+﻿using main.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main.Servesice
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<Schedule> LoadDaete()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<Schedule>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                FileInfo fileInf = new FileInfo(PATH);
+                if (fileInf.Length == 0)
+                {
+                    return new BindingList<Schedule>();
+                }
+                return JsonConvert.DeserializeObject<BindingList<Schedule>>(fileText);
+            }
+        }
+        public void SaveDate(object _todoData)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(_todoData);
+                writer.Write(output);
+            }
+        }
+    }
+}
